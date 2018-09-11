47c679f962d9feaffd1ffaaeba6137a1bb52fe27
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-11 15:18:35 -0500

    created user schema that hashes passwords

---
+++ __tests__/src/model/userModel.tests.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import User from '../../../src/model/userModel';
+import uuid from 'uuid';
+
+const mongoConnect = require('../../../src/util/mongo-connecter');
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+describe('user model', () => {
+  beforeAll(async () => {
+    await mongoConnect(MONGODB_URI);
+  });
+
+  it('saves password as a hashed value', async () => {
+    let password = 'BSD123';
+    let user = new User({
+      username: uuid(),
+      password: password,
+    });
+
+    let newPlayer = await user.save();
+    expect(newPlayer.password).not.toEqual(password);
+    expect( await newPlayer.comparePassword(password)).toBe(newPlayer);
+    expect( await newPlayer.comparePassword('password')).toBe(null);
+    // return newPlayer;
+  });
+});
