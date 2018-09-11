0e542890daf51c47e9c20bb7e130036168cb0d23
JohnMV13 <thejohnmiesterV@gmail.com>
2018-09-11 14:39:31 -0500

    added code for the home page and created a middleware folder that holds an error file and a 404 file

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const express = require('express');
-
-
-
-//import middle ware to add
+import json404 from './src/middleware/404';
+import error from './src/middleware/error';
 
 const app = express();
 
@@ -23,5 +21,20 @@ app.start = (port) =>
             }
         });
 });
+
+app.get('/', (req, res) => {
+  html(res, `<!DOCTYPE html> <html><head><title> Battle Ship Delta </title></head><body><main><p> Welcome to Battle Ship Delta. Please Login Here.</p><a href="/login"> Login </a><p> If you don't have an account, Please Sign Up Here. </p><a href="/signup"> Sign Up </a> <p> Thank you for joining us for this experience.</p></main><footer><div>    &copy; 2018 Battle Ship Delta </div></footer></body></html>`)
+});
+
+app.use(json404);
+app.use(error);
+
+function html(res, content, statusCode=200, statusMessage='OK') {
+  res.statusCode = statusCode;
+  res.statusMessage = statusMessage;
+  res.setHeader('Content-Type', 'text/html');
+  res.write(content);
+  res.end();
+}
 
 module.exports = app;
---
+++ src/middleware/error.js
@@ -0,0 +1,23 @@
+'use strict';
+
+export default (err, req, res, next) => {
+  if(err.name === 'CastError') {
+    res.sendStatus(404);
+    return;
+  }
+  if(err.name === 'ValidationError') {
+    res.statusCode = 400;
+    res.json({
+      message: err.message,
+    });
+    return;
+  }
+  if(req.headers['accept'] !== 'application/json') {
+    next(err);
+    return;
+  }
+  res.statusCode = 500;
+  res.json({
+    error: err.message,
+  });
+}
