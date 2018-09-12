fc71179b344f787fe0db19feda2e15b317804156
JohnMV13 <thejohnmiesterV@gmail.com>
2018-09-12 09:27:41 -0500

    made the links to login and sign up empty

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.start = (port) =>
   });
 
 app.get('/', (req, res) => {
-  html(res, `<!DOCTYPE html> <html><head><title> Battle Ship Delta </title></head><body><main><p> Welcome to Battle Ship Delta. Please Login Here.</p><a href="/login"> Login </a><p> If you don't have an account, Please Sign Up Here. </p><a href="/signup"> Sign Up </a> <p> Thank you for joining us for this experience.</p></main><footer><div>    &copy; 2018 Battle Ship Delta </div></footer></body></html>`);
+  html(res, `<!DOCTYPE html> <html><head><title> Battle Ship Delta </title></head><body><main><p> Welcome to Battle Ship Delta. Please Login Here.</p><a href=""> Login </a><p> If you don't have an account, Please Sign Up Here. </p><a href=""> Sign Up </a> <p> Thank you for joining us for this experience.</p></main><footer><div>    &copy; 2018 Battle Ship Delta </div></footer></body></html>`);
 });
 
 app.use(json404);
