049569e7ddec15d02de522a87bd67816e6362b9e
‘Nathan <ncash13@hotmail.com>
2018-09-13 02:25:31 -0500

    I cannot think of any game functionality we might still be missing, but I know its there

--- src/models/gameModel.js
+++ src/models/gameModel.js
@@ -47,18 +47,60 @@ function arrayLimit(val) {
   return val.length === 2;
 }
 
-// gameSchema.methods.shoot = function(coors){
-//   let p = [
-//     '0: Both players placing ships',
-//     '1: Player 1 placing ships',
-//     '2: Player 2 placing ships',
-//   ];
+gameSchema.methods.gameOverCheck = function(player){
+  let conditions = [];
+  player.ships.forEach(ship =>{
+    if(ship.health === 0){
+      conditions.push(1);
+    }
+  });
+  if(conditions.length === 3){
+    this.phase = '5: Game Over';
+    return `${player.name} lost`;
+  }
+};
 
-//   if(p.includes(this.phase)){
-//     return 'Both players are not ready to play yet';
-//   }
+gameSchema.methods.shoot = function(player, target, coor){
+  let p = [
+    '0: Both players placing ships',
+    '1: Player 1 placing ships',
+    '2: Player 2 placing ships',
+  ];
+  if((p.includes(this.phase))){
+    throw new Error('Both players are not ready to play yet');
+  }
+  let col = coor[0];
+  let row = coor[1];
 
-// }
+  if(typeof coor !== 'string' || coor.length !== 2 || 'a'>col || col>'e' || 1>row || row>5){
+    throw new Error('Unrecognized coordinate');
+  }
+  if(player.board.shotAt.includes(coor)){
+    throw new Error('You shot this spot already');
+  }
+  player.board.shotAt.push(coor);
+  if(target.board.taken.includes(coor)){
+    let sunk;
+    target.ships.forEach(ship =>{
+      let hit;
+      ship.coordinates.forEach(coordinate =>{
+        if (coordinate === coor){
+          console.log('hit');
+          hit = true;
+        }
+      });
+      if(hit){
+        ship.health--;
+        if(ship.health <= 0){
+          sunk = true;
+        }
+      }
+    });
+    if(sunk){
+      return 'A ship was sunk';
+    } else return `${coor} was a hit`;
+  } else return `${coor} was a miss`;
+};
 
 gameSchema.methods.orientationHandler = function(o, start, length){
   let columnS = start[0];
@@ -69,34 +111,34 @@ gameSchema.methods.orientationHandler = function(o, start, length){
   if(o === 'horizontal'){
     if(length > 0){
       for (let i = 0; i<length; i++){
-        let coor = lati + longi;
+        let coor = longi + lati;
         filledSpots.push(coor);
-        longi++;
+        lati--;
       }
     } else{
-      for (let i=0; i<length; i++){
-        let coor = lati + longi;
+      for (let i=0; i>length; i--){
+        let coor = longi + lati;
         filledSpots.push(coor);
-        longi--;
+        lati++;
       }
     }
   }
   else if( o === 'vertical'){
     if(length > 0){
       for (let i = 0; i<length; i++){
-        let coor = lati + longi;
+        let coor = longi + lati;
         filledSpots.push(coor);
-        lati = lati.charCodeAt(0);
-        lati++;
-        lati = String.fromCharCode(lati);
+        longi = longi.charCodeAt(0);
+        longi--;
+        longi = String.fromCharCode(longi);
       }
     } else{
-      for (let i=0; i<length; i++){
-        let coor = lati + longi;
+      for (let i=0; i>length; i--){
+        let coor = longi + lati;
         filledSpots.push(coor);
-        lati = lati.charCodeAt(0);
-        lati--;
-        lati = String.fromCharCode(lati);
+        longi = longi.charCodeAt(0);
+        longi++;
+        longi = String.fromCharCode(longi);
       }
     }
   }
@@ -104,23 +146,27 @@ gameSchema.methods.orientationHandler = function(o, start, length){
 };
 
 gameSchema.methods.placeShips = function(player, start, end){
+  if(!start || !end){
+    throw new Error('No coordinates defined');
+  }
+  if(typeof start !== 'string' || typeof end !== 'string'){
+    throw new Error('Cannot read coordinates. Coordinates must be strings.');
+  }
   let p = [
     '0: Both players placing ships',
     '1: Player 1 placing ships',
     '2: Player 2 placing ships',
   ];
   if(!(p.includes(this.phase))){
-    return 'Ships have already been placed';
+    throw new Error('Ships have already been placed');
   }
-  if(!start){
-    return 'Please include coordinates for your ship';
+  if(start.length !== 2 || end.length !== 2){
+    throw new Error('Unrecognized coordinate, try letters a-e and numbers 1-5');
   }
-  if(start.length !== 2){
-    return 'Unrecognized coordinate, try letters a-e and numbers 1-5';
-  }
-  let targetPlayer = this.players.forEach(target=>{
+  let targetPlayer; 
+  this.players.forEach(target=>{
     if(player === target){
-      return target;
+      targetPlayer = target;
     }
   });
   let targetBoard = targetPlayer.board;
@@ -128,38 +174,49 @@ gameSchema.methods.placeShips = function(player, start, end){
   let rowS = start[1];
   let columnE = end[0];
   let rowE = end[1];
-  if(columnS > 'e' || rowS > 5 || columnE > 'e' || rowE > 5){
-    return 'Invalid position given';
+  if('a'> columnS || columnS > 'e' || 1 > rowS || rowS > 5 || 'a'>columnE || columnE > 'e' || 1>rowE || rowE > 5){
+    throw new Error('Invalid position given');
   }
   if(columnS !== columnE && rowS !== rowE){
-    return 'No diagonal placements allowed';
+    throw new Error('No diagonal placements allowed');
   }
-  if(targetBoard.taken.includes(start) || targetBoard.taken.include(end)){
-    return 'One of more of these positions are not available';
+  let taken = targetBoard.taken;
+  if(taken.includes(start) || taken.includes(end)){
+    throw new Error('One of more of these positions are not available');
   }
 
   let orientation;
   let shipLength;
   if(columnS === columnE){
     orientation = 'horizontal';
-    shipLength = columnS.charCodeAt(0) - columnE.charCodeAt(0);
+    shipLength = rowS - rowE;
   } else {
     orientation = 'vertical';
-    shipLength = rowS - rowE;
+    shipLength = columnS.charCodeAt(0) - columnE.charCodeAt(0);
   }
-  if(shipLength > 4 || shipLength < 2){
-    return 'Invalid ship length';
+  if(shipLength>0){
+    shipLength++;
+  }
+  if(shipLength<0){
+    shipLength--;
+  }
+  if((shipLength > 4 || shipLength < 2) && (shipLength< -4 || shipLength > -2)){
+    throw new Error('Ship length invalid');
   }
   let coordinates = this.orientationHandler(orientation, start, shipLength);
   coordinates.forEach(coor =>{
-    if(this.targetPlayer.board.taken.includes(coor)){
-      return 'One of your coordinates is already taken';
+    if(taken.includes(coor)){
+      throw new Error('One or more of your coordinates is already taken');
     } else{
-      this.targetPlayer.board.taken.push(coor);
+      taken.push(coor);
     }
   });
-  this.targetPlayer.ships.forEach(ship =>{
+  shipLength = Math.sqrt(Math.pow(shipLength, 2));
+  targetPlayer.ships.forEach(ship =>{
     if(ship.length === shipLength){
+      if(ship.placed){
+        throw new Error(`Your ship of length ${shipLength} has already been placed`);
+      }
       ship.coordinates = coordinates;
       ship.placed = true;
     }
@@ -177,68 +234,101 @@ gameSchema.methods.phaseChange = function(player, coors){
     '5: Game Over',
   ];
   if(this.phase === p[0]){
-    if(player === this.players[0]){
-      this.placeShips(player, coors[0], coors[1]);
-      let notAllPlaced = player.ships.forEach(ship=>{
-        if(!ship.placed){
-          return true;
-        }
-      });
-      if(!notAllPlaced){
-        this.phase = p[2];
+    let result = this.placeShips(player, coors[0], coors[1]);
+    let notAllPlaced;
+    player.ships.forEach(ship=>{
+      if(!ship.placed){
+        notAllPlaced = true;
       }
-      return this.phase;
-    } else {
-      this.placeShips(player, coors[0], coors[1]);
-      let notAllPlaced = player.ships.forEach(ship=>{
-        if(!ship.placed){
-          return true;
-        }
-      });
-      if(!notAllPlaced){
+    });
+    if(!notAllPlaced){
+      if(player === this.players[0]){
+        this.phase = p[2];
+      } else{
         this.phase = p[1];
       }
-      return this.phase;
     }
-  } else if(this.phase === p[3]){
+    return (`${result} | ${this.phase}`);
+  }
+  else if(this.phase === p[3]){
+    let target = this.players[1];
+    let current = this.players[0];
+    let result = this.shoot(current, target, coors[0]);
     this.phase = p[4];
-    return this.phase;
+    if(result === 'A ship was sunk'){
+      let query = this.gameOverCheck(target);
+      if(query){
+        result = query;
+      }
+    }
+    return (`${result} | ${this.phase}`);
   } else if(this.phase === p[4] || this.phase === p[1] || this.phase === p[2]){
+    let result;
     if(this.phase !== p[4]){
-      this.placeShips(player, coors[0], coors[1]);
-      let notAllPlaced = player.ships.forEach(ship=>{
+      result = this.placeShips(player, coors[0], coors[1]);
+      let notAllPlaced;
+      player.ships.forEach(ship=>{
         if(!ship.placed){
-          return true;
+          notAllPlaced = true;
         }
       });
       if(!notAllPlaced){
         this.phase = p[3];
       }
     } else{
+      let target = this.players[0];
+      let current = this.players[1];
+      let result = this.shoot(current, target, coors[0]);
       this.phase = p[3];
+      if(result === 'A ship was sunk'){
+        let query = this.gameOverCheck(target);
+        if(query){
+          result = query;
+        }
+      }
+      return(`${result} | ${this.phase}`);
     }
-    return this.phase;
+    return (`${result} | ${this.phase}`);
   } else if(this.phase === p[5]){
     return 'The game is over. GO AWAY! <3';
   }
 };
 
 gameSchema.methods.turnHandler = function(player, ...coors){
+  let result;
+  if(!player){
+    throw new Error('Player not defined');
+  }
   let name = player.name;
   if(name === this.players[0].name || name === this.players[1].name){
     if(player.isTurn !== true){
-      return 'Not your turn';
+      throw new Error('Not your turn');
     }
-    this.phaseChange(player, coors);
-    this.players.forEach(player => {
-      if(player.name === name){
-        player.isTurn = false;
-      }else{
-        player.isTurn = true;
-      }
-    });
+    result = this.phaseChange(player, coors);
+    let p = [
+      '0: Both players placing ships',
+      '1: Player 1 placing ships',
+      '2: Player 2 placing ships',
+      '3: Player 1s turn',
+      '4: Player 2s turn',
+      '5: Game Over',
+    ];
+    if(this.phase === p[0]){
+      this.players.forEach(player => player.isTurn = true);
+    }
+    if(this.phase === p[1] || this.phase === p[3]){
+      this.players[0].isTurn = true;
+      this.players[1].isTurn = false;
+    }
+    if(this.phase === p[2] || this.phase === p[4]){
+      this.players[0].isTurn = false;
+      this.players[1].isTurn = true;
+    }
+    return ( `${result} | ${player.name}'s turn was processed.`);
   }
-  return `${player.name}'s turn was changed.`;
+  else{
+    throw new Error('You are not a part of this game');
+  }
 };
 
 
