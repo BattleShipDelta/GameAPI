869f3fc486c7a47622481dacd9d90161b83d7ea4
Dylan Schroeder <schrodac@gmail.com>
2018-09-13 21:21:21 -0500

    add fire route and tests

--- __tests__/src/routes/gameRoute.tests.js
+++ __tests__/src/routes/gameRoute.tests.js
@@ -2,6 +2,8 @@
 
 import app from '../../../app';
 import User from '../../../src/model/userModel';
+import Game from '../../../src/models/gameModel';
+import Player from '../../../src/constructors/game';
 const request = require('supertest')(app);
 const mongoConnect = require('../../../src/util/mongo-connect');
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -54,4 +56,71 @@ describe('an invite', ()=> {
       });
       
   });
+  describe('move route', () => {
+    let game;
+    let opponentToken;
+
+    beforeEach(async() => {
+      let p1 = new Player(user.username);
+      let p2 = new Player(opponent.username);
+      game = Game.start(p1,p2);
+      await game.save();
+      opponentToken = user.generateToken();
+    });
+    afterEach(async()=> {
+      await Game.deleteOne({_id: game._id});
+    });
+    it('return 403(forbidden) when ships not placed yet', async() => {
+      await request
+        .post(`/api/games/${game._id}/move`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({coors:'a1'})
+        .expect(403);
+    });
+    describe('with ships', ()=>{
+      beforeEach(()=> {
+        game.turnHandler(game.players[0], 'c1', 'b1');
+        game.turnHandler(game.players[0], 'c2', 'e2');
+        game.turnHandler(game.players[0], 'a4', 'a1');
+        game.turnHandler(game.players[1], 'c1', 'b1');
+        game.turnHandler(game.players[1], 'c2', 'e2');
+        game.turnHandler(game.players[1], 'a4', 'a1');
+        game.save();
+      });
+      it('fires successfully', async()=> {
+        await request
+          .post(`/api/games/${game._id}/move`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({coors:'a1'})
+          .expect(200)
+          .expect({ result: 'a1 was a hit | 4: Player 2s turn | Etahn\'s turn was processed.'});
+        await request
+          .post(`/api/games/${game._id}/move`)
+          .set('Authorization', `Bearer ${opponentToken}`)
+          .send({coors:'e5'})
+          .expect(200)
+          //Shouldn't be Etahn's turn
+          .expect({ result: 'e5 was a miss | 4: Player 2s turn | Etahn\'s turn was processed.' });
+      });
+      //Bug: turnHandler doesn't check players turn?
+      it.skip('returns 403(forbidden) when you play out of turn', async()=> {
+
+        await request
+          .post(`/api/games/${game._id}/move`)
+          .set('Authorization', `Bearer ${opponentToken}`)
+          .send({coors:'a1'})
+          .expect(403);
+      });
+    });
+  
+    //Error 'no coordinates defined' is not specific enough?
+    it.skip('returns 400(bad request) when a player shoots off the board', async()=> {
+      await request
+        .post(`/api/games/${game._id}/move`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({coors:'a11'})
+        .expect(400);
+    });
+  });
+    
 });
--- src/routes/gameRoute.js
+++ src/routes/gameRoute.js
@@ -7,6 +7,7 @@ import Player from '../constructors/game';
 import Game from '../models/gameModel';
 import User from '../model/userModel';
 import auth from '../middleware/auth-middleware';
+import { request } from 'http';
 
 router.post('/games', auth, async(req,res) => {
   let p1 = new Player(req.user.username);
@@ -23,6 +24,29 @@ router.post('/games', auth, async(req,res) => {
     res.send(400);
   }
   
+});
+
+router.post('/games/:id/move', auth, async(req, res)=>{
+  console.log('move route');
+  let game = await Game.findById(req.params.id);
+  console.log(game);
+  console.log(req.body);
+  console.log(req.body.coors);
+  let player = game.players.find(player => player.name === req.user.username);
+  try{
+    let result = game.turnHandler(player, req.body.coors);
+    console.log(result);
+    res.send(200, { result });
+    
+    
+  }
+  catch(error){
+    console.log(game.phase);
+    console.log(error.message);
+    res.send(403, { error: error.message });
+  }
+  res.send(500);
+
 });
 
 export default router;
